0534c7d8190ba42ab171252ffb597dd6ed5cdb6b
Nguyen Vu Nguyen <nvunguyen@gmail.com>
2013-09-29 19:42:29 +0800

    Use remote_url for resources

--- resources/app/models/refinery/resource.rb
+++ resources/app/models/refinery/resource.rb
@@ -12,7 +12,7 @@ module Refinery
     # Docs for acts_as_indexed http://github.com/dougal/acts_as_indexed
     acts_as_indexed :fields => [:file_name, :title, :type_of_content]
 
-    delegate :ext, :size, :mime_type, :url, :to => :file
+    delegate :ext, :size, :mime_type, :url, :remote_url, :to => :file
 
     # used for searching
     def type_of_content
